5fb83f1f61e7296f205882e6104a0edb0c536f40
Kayden Lee <kayden@programmer.net>
2022-08-24 23:23:03 +0000

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,7 +19,7 @@ const NavBar = () => {
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact Us</a></li>
-                <li><button onclick="goToLink(https://google.com)" class="login-button" href="/login">Login</button></li>
+                <li><button onclick="goToLink(https://google.com)" class="login-button">Login</button></li>
                 <li><button class="join-button" href="/signup">Join</button></li>
             </ul>
         </nav>
