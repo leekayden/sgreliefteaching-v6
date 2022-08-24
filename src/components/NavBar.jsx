0debd755dbc9e85b32075947c0216556aa7a8b2c
Kayden Lee <kayden@programmer.net>
2022-08-24 23:18:41 +0000

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const NavBar = () => {
+  function goToLink(link) {
+    location.href = link;
+  }
   return (
     <div>
         <nav>
@@ -16,7 +19,7 @@ const NavBar = () => {
                 <li><a href="/">Home</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact Us</a></li>
-                <li><button class="login-button" href="/login">Login</button></li>
+                <li><button onclick="goToLink(https://google.com)" class="login-button" href="/login">Login</button></li>
                 <li><button class="join-button" href="/signup">Join</button></li>
             </ul>
         </nav>
