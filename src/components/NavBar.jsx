55370bf11d2a312b6c9ef577ea1773aa41b317fc
Kayden Lee <kayden@programmer.net>
2022-08-25 01:43:41 +0000

    update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
-  function goToLink(link) {
-    window.location.href = link;
-  }
   return (
     <div>
-        <nav>
-            <div className="logo">
-                <img src="https://cdn.cloudservetechcentral.com/sgreliefteaching/logo-lg.png" alt="SGreliefteaching Logo"/>
-            </div>
-            <div className="hamburger">
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-            </div>
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-                <li><button onclick="goToLink(https://google.com)" class="login-button">Login</button></li>
-                <li><button class="join-button" href="/signup">Join</button></li>
-            </ul>
-        </nav>
+      <nav>
+        <div className="logo">
+          <img src="https://cdn.cloudservetechcentral.com/sgreliefteaching/logo-lg.png" alt="SGreliefteaching Logo" />
+        </div>
+        <div className="hamburger">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact Us</a></li>
+          <Link to='https://react.semantic-ui.com/'>
+            <li><button onclick="goToLink(https://google.com)" class="login-button">Login</button></li>
+          </Link>
+          <li><button class="join-button" href="/signup">Join</button></li>
+        </ul>
+      </nav>
     </div>
   )
 }
