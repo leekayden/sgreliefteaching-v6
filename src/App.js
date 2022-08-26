6176a3ef26b4e7086ec1f3e66d50d2f8f6183e0d
Kayden Lee <kayden@programmer.net>
2022-08-26 09:11:42 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div></div>
+    <div>
+        <NavBar />
+    </div>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div>
-        <nav>
-            <div className="logo">
-                <a href="/" rel="noopener noreferrer"><img src="https://cdn.cloudservetechcentral.com/sgreliefteaching/logo-lg.png" alt="SGreliefteaching Logo"/></a>
-            </div>
-            <div className="hamburger">
-                <div className="line1"></div>
-                <div className="line2"></div>
-                <div className="line3"></div>
-            </div>
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-                <li><button className="login-button" href="/login">Login</button></li>
-                <li><button className="join-button" href="/signup">Join</button></li>
-            </ul>
-        </nav>
+      <nav>
+        <div className="logo">
+          <a href="/" rel="noopener noreferrer">
+            <img
+              src="https://cdn.cloudservetechcentral.com/sgreliefteaching/logo-lg.png"
+              alt="SGreliefteaching Logo"
+            />
+          </a>
+        </div>
+        <div className="hamburger">
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact Us</a>
+          </li>
+          <Link to="/login">
+            <li>
+              <button className="login-button">
+                Login
+              </button>
+            </li>
+          </Link>
+          <li>
+            <button className="join-button" href="/signup">
+              Join
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
